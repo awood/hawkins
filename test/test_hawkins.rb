3c8469336624572ac95e11cf773e07692da4b7f0
Alex Wood <awood@redhat.com>
2014-12-23 15:53:56 -0500

    Add a unit test for creating a post.

--- test/test_hawkins.rb
+++ test/test_hawkins.rb
@@ -26,6 +26,23 @@ module Hawkins
         end
         expect(err).to match(/called with no arguments/)
       end
+
+      it 'creates a post for specific date' do
+        title = "Party Like It's 1999"
+        expected_body =<<-BODY.gsub(/^\s*/,'')
+        ---
+        title: #{title}
+        ---
+        BODY
+        expected_file="_posts/1999-12-31-#{title.to_url}.md"
+        Cli.no_commands do
+          Cli.any_instance.expects(:empty_directory)
+          Cli.any_instance.expects(:create_file).with(expected_file, expected_body)
+          Cli.any_instance.expects(:exec).with('foo', expected_file)
+        end
+
+        Cli.start(%W(post --editor foo --date 1999-12-31 #{title}))
+      end
     end
   end
 end
